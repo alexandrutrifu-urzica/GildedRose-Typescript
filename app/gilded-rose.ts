290dac118332d001a0c302d64c3011e32cb3f7b9
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-04 14:50:12 +0300

    refactored quality changes

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,6 +19,10 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
+            const maxQualityCondition = (this.items[i].quality < 50)
+            const positiveQualityCondition = (this.items[i].quality > 0)
+
+            // Quality changes
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -42,25 +46,30 @@ export class GildedRose {
                     }
                 }
             }
+
+            // SellIn Value Changes
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+                this.items[i].sellIn -= 1;
             }
+
+            // Handle negative sellIn values
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
+                if (this.items[i].name == 'Aged Brie' && maxQualityCondition) {
+                    this.items[i].quality = this.items[i].quality + 1
+                    continue
                 }
+
+                if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    this.items[i].quality = 0
+                    continue
+                }
+
+                if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+                    continue
+                }
+
+                // Common item (regular behavior)
+                this.items[i].quality = positiveQualityCondition ? this.items[i].quality - 1 : 0
             }
         }
 
